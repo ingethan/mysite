7a5110d8736620766082d28f7d6574d29cb7bc4d
ethaning315 <ethaning@hotmail.com>
2020-07-10 22:51:54 +0800

    2235

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Django.views.generic.base import RedirectView
+from django.views.generic.base import RedirectView
 from app.views import hello, index
 
 urlpatterns = [
